a9ab31fbde1d4ab01f4e98b5afd6cfd93b3191ad
Clippy <68430534+Clippy29@users.noreply.github.com>
2021-06-09 00:23:50 +0200

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,13 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+files {
+	'files/sound/intro.ogg',
+	'files/images/*.gif',
+	'files/images/*.txt',
+	'files/images/*.jpg',
+	'files/images/*.png',
+	'files/js/index.js',
+	'files/loading.html'
+}
+
+loadscreen 'files/loading.html'
